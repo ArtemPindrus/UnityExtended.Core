db2e42f31d70b95858209abb6f2d8fffd68bd343
Artem Pindrus <138709555+ArtemPindrus@users.noreply.github.com>
2024-08-05 20:28:17 +0300

    Initial Commit

---
+++ Runtime/Extensions/VectorExtensions.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+namespace UnityExtended.Extensions {
+    /// <summary>
+    /// Provides additional functionality to the <see cref="Vector3"/> class.
+    /// </summary>
+    public static class VectorExtensions {
+        /// <summary>
+        /// Modifies a <see cref="Vector3"/> to have given components. 
+        /// If a component is assigned a NaN - it'll get ignored.
+        /// </summary>
+        /// <param name="v">Modified <see cref="Vector3"/>.</param>
+        /// <param name="x">New value of the x component.</param>
+        /// <param name="y">New value of the y component.</param>
+        /// <param name="z">New value of the z component.</param>
+        /// <returns>Modified <see cref="Vector3"/>.</returns>
+        public static Vector3 With(this Vector3 v, float x = float.NaN, float y = float.NaN, float z = float.NaN) {
+            Vector3 result = v;
+            if (x != float.NaN) result.x = x;
+            if (y != float.NaN) result.y = y;
+            if (z != float.NaN) result.z = z;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Adds given values to corresponding components of a <see cref="Vector3"/>.
+        /// </summary>
+        /// <param name="v">Modified <see cref="Vector3"/>.</param>
+        /// <param name="x">Value added to the x component.</param>
+        /// <param name="y">Value added to the y component.</param>
+        /// <param name="z">Value added to the z component.</param>
+        /// <returns>Modified <see cref="Vector3"/>.</returns>
+        public static Vector3 Add(this Vector3 v, float x = 0, float y = 0, float z = 0) {
+            Vector3 result = v;
+            result.x += x;
+            result.y += y;
+            result.z += z;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Adds a value to all the components of a <see cref="Vector3"/>.
+        /// </summary>
+        /// <param name="v">Modified <see cref="Vector3"/>.</param>
+        /// <param name="value">Value added to every component of <paramref name="v"/>.</param>
+        /// <returns></returns>
+        public static Vector3 Add(this Vector3 v, float value) {
+            Vector3 result = v;
+            result.x += value;
+            result.y += value;
+            result.z += value;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Projects a vector onto <paramref name="projectionVector"/> with ability to clamp it to both ends.
+        /// </summary>
+        /// <param name="modifiedVector">Modified <see cref="Vector3"/>.</param>
+        /// <param name="projectionVector"><see cref="Vector3"/> on which to project.</param>
+        /// <param name="clampTail">If the projection results in a vector of the opposite direction to the <paramref name="projectionVector"/>, whether to return zero vector.</param>
+        /// <param name="clampHead">if the projection results in a vector longer then the <paramref name="projectionVector"/>, whether to clamp resulting vector to the length of <paramref name="projectionVector"/>.</param>
+        /// <returns></returns>
+        public static Vector3 ProjectClamped(this Vector3 modifiedVector, Vector3 projectionVector, bool clampTail = true, bool clampHead = true) {
+            float length = projectionVector.magnitude;
+            Vector3 direction = projectionVector.normalized;
+
+            float dot = Vector3.Dot(modifiedVector, direction);
+            float scalar = dot;
+
+            if (clampTail && dot < 0) scalar = 0;
+            else if (clampHead && dot > length) scalar = length;
+
+            return direction * scalar;
+        }
+    }
+}
---
+++ Runtime/Utilities/RigidbodyU/LineConstraint.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityExtended.Extensions;
+
+namespace UnityExtended.Utilities.RigidbodyU {
+    [RequireComponent(typeof(Rigidbody))]
+    /// <summary>
+    /// Component that allows to restrict rigidbody movement to the line betweeb two points.
+    /// </summary>
+    public class LineConstraint : MonoBehaviour {
+        private const float DebugSpheresRadius = 0.1f;
+
+        [SerializeField]
+        [Tooltip("Visualize two endpoints and the line between them.")]
+        private bool debug;
+        [SerializeField]
+        private Vector3 firstPoint;
+        [SerializeField]
+        private Vector3 secondPoint;
+
+        private Rigidbody rb;
+
+        /// <summary>
+        /// Gets a vector pointing from <see cref="firstPoint"/> to the <see cref="secondPoint"/>.
+        /// </summary>
+        public Vector3 FirstToSecond { get; private set; }
+
+        /// <summary>
+        /// Gets normalized <see cref="FirstToSecond"/>.
+        /// </summary>
+        public Vector3 FirstToSecondDir { get; private set; }
+
+        private void Awake() {
+            rb = GetComponent<Rigidbody>();
+
+            FirstToSecond = secondPoint - firstPoint;
+            FirstToSecondDir = FirstToSecond.normalized;
+        }
+
+        private void FixedUpdate() {
+            AllignPosition();
+            RestrictVelocity();
+        }
+
+        private void OnDrawGizmos() {
+            if (debug) {
+                Gizmos.DrawWireSphere(secondPoint, DebugSpheresRadius);
+                Gizmos.DrawWireSphere(firstPoint, DebugSpheresRadius);
+
+                Debug.DrawLine(firstPoint, secondPoint);
+            }
+        }
+
+        private void AllignPosition() {
+            Vector3 firstToBody = transform.position - firstPoint;
+            Vector3 aligned = firstToBody.ProjectClamped(FirstToSecond);
+
+            rb.position = firstPoint + aligned;
+        }
+
+        private void RestrictVelocity() {
+            Vector3 currentPosition = rb.position;
+            Vector3 projectedVelocity = Vector3.Project(rb.velocity, FirstToSecond);
+            Vector3 projectedDirection = projectedVelocity.normalized;
+
+            if (currentPosition == firstPoint && projectedDirection == -FirstToSecondDir) {
+                projectedVelocity = Vector3.zero;
+            } else if (currentPosition == secondPoint && projectedDirection == FirstToSecondDir) {
+                projectedVelocity = Vector3.zero;
+            }
+
+            rb.velocity = projectedVelocity;
+        }
+    }
+}
