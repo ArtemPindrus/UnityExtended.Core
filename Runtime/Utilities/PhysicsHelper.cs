63739ba99a923beaa50b655c2d939ee0c0c65038
Artem Pindrus <artempindrus@gmail.com>
2024-08-08 12:47:28 +0300

    Docs update

--- Runtime/Utilities/PhysicsHelper.cs
+++ Runtime/Utilities/PhysicsHelper.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 namespace UnityExtended.Utilities {
     public static class PhysicsHelper {
         /// <summary>
-        /// Predicts velocity of a <see cref="Rigidbody"/> after application of accumulated forces, but before the collision forces.
-        /// <para>SHOULD BE USED ONLY IN FIXED UPDATE (therefore before internal physics update).</para>
+        /// Predicts linear velocity of a <see cref="Rigidbody"/> on the next physics update after application of accumulated forces, but before the collision forces.
+        /// <para>INTENDED TO BE USED ONLY IN FIXED UPDATE (therefore before internal physics update).</para>
         /// <para>When tested, method provided exclusively accurate results (predictions coincided with actual forces after internal physics update), but take in mind that I may have missed some important point about force application.</para>
         /// </summary>
-        /// <param name="rb"></param>
-        /// <param name="accountDrag">Whether to account rigidbody drag into calculations. Set to true for accuracy.</param>
+        /// <param name="rb"><see cref="Rigidbody"/>, linear velocity of which is calculated.</param>
+        /// <param name="accountDrag">Whether to account rigidbody's drag into calculations. Set to true for accuracy.</param>
         /// <param name="accountGravity">Whether to account rigidbody gravity into calculations. Set to true for accuracy.</param>
-        /// <returns>Velocity of a rigidbody after application of accumulate forces in the internal physics update.</returns>
-        public static Vector3 PredictRBVelocityPostAccumulated(Rigidbody rb, bool accountDrag = true, bool accountGravity = true) {
+        /// <returns>Linear velocity of a rigidbody after application of accumulated forces in the internal physics update.</returns>
+        public static Vector3 PredictLinearVelocityPostAccumulated(Rigidbody rb, bool accountDrag = true, bool accountGravity = true) {
             float fixedDT = Time.fixedDeltaTime;
 
             Vector3 accumulatedForce = rb.GetAccumulatedForce();
