73342f8c3f1c8c416a10c444b8c3a49d1f5739aa
Artem Pindrus <artempindrus@gmail.com>
2024-09-21 20:33:22 +0300

    StickDown

---
+++ Utilities/StickDown.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets {
+    /// <summary>
+    /// In-editor tool to stick an object (with colliders) to the ground.
+    /// </summary>
+    public class StickDown : MonoBehaviour {
+#if UNITY_EDITOR
+        private void Reset() {
+            Stick();
+        }
+
+        [ContextMenu(nameof(Stick))]
+        private void Stick() {
+            Physics.Raycast(transform.position, Vector3.down, out RaycastHit hitDown);
+
+            Physics.Raycast(hitDown.point, Vector3.up, out RaycastHit traced);
+
+            Vector3 difference = transform.position - traced.point;
+
+            transform.position = hitDown.point + difference;
+
+            DestroyImmediate(this);
+        }
+    }
+#endif
+}
