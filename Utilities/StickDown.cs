1c0d4ba172d22437079543499b68b67489aa9704
Artem Pindrus <artempindrus@gmail.com>
2024-09-21 20:37:15 +0300

    Namespace change

--- Utilities/StickDown.cs
+++ Utilities/StickDown.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace Assets {
+namespace UnityExtended.Core.Utilities {
     /// <summary>
     /// In-editor tool to stick an object (with colliders) to the ground.
     /// </summary>
@@ -12,9 +12,9 @@ namespace Assets {
 
         [ContextMenu(nameof(Stick))]
         private void Stick() {
-            Physics.Raycast(transform.position, Vector3.down, out RaycastHit hitDown);
+            UnityEngine.Physics.Raycast(transform.position, Vector3.down, out RaycastHit hitDown);
 
-            Physics.Raycast(hitDown.point, Vector3.up, out RaycastHit traced);
+            UnityEngine.Physics.Raycast(hitDown.point, Vector3.up, out RaycastHit traced);
 
             Vector3 difference = transform.position - traced.point;
 
