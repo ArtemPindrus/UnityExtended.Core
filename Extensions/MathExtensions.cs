27d0401f3ff0b66df3053110c2f25999aebecc88
Artem Pindrus <artempindrus@gmail.com>
2024-10-02 15:16:13 +0300

    SingZero for int

--- Extensions/MathExtensions.cs
+++ Extensions/MathExtensions.cs
@@ -16,6 +16,17 @@ namespace UnityExtended.Core.Extensions {
         };
 
         /// <summary>
+        /// Just like Math.Sign() but returns 0 if the given value is zero.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>1 if <paramref name="number"/> is greater then 0, -1 if it's less then 0 or 0 if it's 0</returns>
+        public static int SignZero(this int number) => number switch {
+            0 => 0,
+            > 0 => 1,
+            < 0 => -1,
+        };
+
+        /// <summary>
         /// Keeps value in [-180; 180] range, simulating overflow.
         /// <para>For example, for (value: 181, min: -180, max: 180) method will return -179 (value overflows max into the min by one).</para>
         /// <para>Used for keeping the value returned from eulerAngles.(Component) in [-180; 180] range.</para>
@@ -34,8 +45,12 @@ namespace UnityExtended.Core.Extensions {
         /// <param name="min">Min value of the range. Should be the least number in the range.</param>
         /// <param name="max">Max value of the range. Should be the greatest number in the range.</param>
         /// <returns>Overflowed <paramref name="value"/>.</returns>
+        /// <exception cref="Exception">Passed arguments for <paramref name="min"/> and <paramref name="max"/> are swapped or equal.</exception>
         public static float RangeOverflowExclusive(float value, float min, float max) {
             float range = max - min;
+
+            if (range <= 0) throw new Exception("Invalid range. Ensure the order of min and max arguments and that they aren't equal.");
+
             float clamped = value;
 
             if (value > max) {
