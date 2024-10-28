5517effd5920ff2f08662de9401a649fe82a8d1a
Artem Pindrus <artempindrus@gmail.com>
2024-10-28 17:24:10 +0200

    Cleanup

--- EditorSceneTools/EditorSceneToolsInitializer.cs
+++ EditorSceneTools/EditorSceneToolsInitializer.cs
@@ -4,23 +4,23 @@ using System.Reflection;
 using UnityEngine;
 
 namespace UnityExtended.Core.EditorSceneTools {
-	public class EditorSceneToolsInitializer {
+    public class EditorSceneToolsInitializer {
 #if UNITY_EDITOR
-		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-		public static void InitializeTools() {
-			Assembly assembly = Assembly.GetExecutingAssembly();
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void InitializeTools() {
+            Assembly assembly = Assembly.GetExecutingAssembly();
 
-			Type targetType = typeof(IEditorSceneTool);
+            Type targetType = typeof(IEditorSceneTool);
 
-			var tools = assembly
-				.GetTypes()
-				.Where(t => targetType.IsAssignableFrom(t) && t.IsClass && !t.IsAbstract);
+            var tools = assembly
+                .GetTypes()
+                .Where(t => targetType.IsAssignableFrom(t) && t.IsClass && !t.IsAbstract);
 
-			foreach (var t in tools) {
-				var component = new GameObject(t.Name).AddComponent(t);
-				UnityEngine.Object.DontDestroyOnLoad(component);
-			}
-		}
+            foreach (var t in tools) {
+                var component = new GameObject(t.Name).AddComponent(t);
+                UnityEngine.Object.DontDestroyOnLoad(component);
+            }
+        }
 #endif
-	}
+    }
 }
