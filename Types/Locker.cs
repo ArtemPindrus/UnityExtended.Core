27af65e57a4a5b29634d7723b428f3acc0ebace8
Artem Pindrus <artempindrus@gmail.com>
2024-10-28 17:23:03 +0200

    Create Locker.cs

---
+++ Types/Locker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace UnityExtended.Core.Types {
+    public class Locker {
+        private readonly HashSet<object> locks = new();
+
+        public bool IsLocked => locks.Count > 0;
+
+        public void Lock(object @lock, bool toLock) {
+            if (toLock) Lock(@lock);
+            else ReleaseLock(@lock);
+        }
+
+        public void Lock(object @lock) {
+            locks.Add(@lock);
+        }
+
+        public void ReleaseLock(object @lock) {
+            locks.Remove(@lock);
+        }
+
+        public static implicit operator bool(Locker locker) => locker.IsLocked;
+    }
+}
