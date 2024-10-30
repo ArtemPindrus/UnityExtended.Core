094a51564755bc3bf45281838ee6ab9b3c96bd0a
Artem Pindrus <artempindrus@gmail.com>
2024-10-30 10:51:40 +0200

    Rename

--- Extensions/AudioSourceExtensions.cs
+++ Extensions/AudioSourceExtensions.cs
@@ -5,11 +5,14 @@ using UnityEngine.Audio;
 #nullable enable
 namespace UnityExtended.Core.Extensions {
     public static class AudioSourceExtensions {
-        public static void PlayRandomResource(this AudioSource source, AudioResource[] resources) {
-            int randomInd = Random.Range(0, resources.Length);
-            source.resource = resources[randomInd];
 
             source.Play();
         }
+
+        public static void PlayRandom(this AudioSource source, AudioResource[] resources) {
+            int randomInd = Random.Range(0, resources.Length);
+            source.Play(resources[randomInd]);
+        }
+
     }
 }
