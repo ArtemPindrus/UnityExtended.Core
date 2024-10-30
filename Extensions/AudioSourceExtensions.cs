63da0d318148953e1ffa26a6ab53c97463f2fc37
Artem Pindrus <artempindrus@gmail.com>
2024-10-30 10:51:14 +0200

    nullable enable

--- Extensions/AudioSourceExtensions.cs
+++ Extensions/AudioSourceExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;
 
+#nullable enable
 namespace UnityExtended.Core.Extensions {
     public static class AudioSourceExtensions {
         public static void PlayRandomResource(this AudioSource source, AudioResource[] resources) {
