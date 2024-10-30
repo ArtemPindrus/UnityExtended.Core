06e15508b2411c7b7abfdffa4d498f6d11bd533f
Artem Pindrus <artempindrus@gmail.com>
2024-10-30 18:10:07 +0200

    Reduction

---
+++ Types/Reducer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace FirstPersonController {
+    public class Reducer {
+        private float reductionPercentage;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Reducer"/> class.
+        /// </summary>
+        /// <param name="percentageOfReduction">Initial percentage of sensitivity reduction.</param>
+        public Reducer(float percentageOfReduction) {
+            ReductionPercentage = percentageOfReduction;
+        }
+
+        /// <summary>
+        /// Gets or sets percentage of sensitivity to reduce it by. A value in [0;1] range.
+        /// </summary>
+        public float ReductionPercentage {
+            get => reductionPercentage;
+            set {
+                reductionPercentage = Mathf.Clamp01(value);
+            }
+        }
+
+        public override string ToString() => $"Reducer with {reductionPercentage} value.";
+    }
+}
---
+++ Types/Reduction.cs
@@ -0,0 +1,31 @@
+﻿using FirstPersonController;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityExtended.Core.Types {
+    public class Reduction {
+        private readonly List<Reducer> reducers = new();
+
+        public float SumReductionPercentage { 
+            get {
+                float sumReductionPercentage = 0;
+
+                foreach (var reducer in reducers) {
+                    sumReductionPercentage += reducer.ReductionPercentage;
+                }
+
+                return Mathf.Clamp01(sumReductionPercentage);
+            }    
+        }
+
+        public float SumLeftPercentage => 1 - SumReductionPercentage;
+
+        public void AddReducer(float percentageOfReduction) => reducers.Add(new(percentageOfReduction));
+
+        public void RemoveReducer(Reducer reducer) => reducers.Remove(reducer);
+
+        public float Reduce(float value) => value * SumLeftPercentage;
+
+        public int Reduce(int value) => (int)(value * SumLeftPercentage);
+    }
+}
