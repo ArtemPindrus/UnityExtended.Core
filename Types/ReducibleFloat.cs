24ed82631250bd7093ca77453420407df9bbbbd6
Artem Pindrus <artempindrus@gmail.com>
2024-10-31 10:02:41 +0200

    Create ReducibleFloat.cs

---
+++ Types/ReducibleFloat.cs
@@ -0,0 +1,33 @@
+﻿using EditorAttributes;
+using System;
+using UnityEngine;
+
+namespace UnityExtended.Core.Types {
+    [Serializable]
+    public class ReducibleFloat {
+        private Reduction reduction;
+
+        [field: SerializeField]
+        public float ValueUnchanged { get; private set; }
+
+        /// <summary>
+        /// Gets reduced value.
+        /// </summary>
+        public float Value => reduction.Reduce(ValueUnchanged);
+
+        public ReducibleFloat(float initialValue) { 
+            reduction = new();   
+            ValueUnchanged = initialValue;
+        }
+
+        public Reducer AddReducer(float reductionPercentage) {
+            reduction ??= new();
+
+            return reduction.AddReducer(reductionPercentage);
+        }
+
+        public void RemoveReducer(Reducer reducer) => reduction.RemoveReducer(reducer);
+
+        private void LogReduced() => Debug.Log(Value);
+    }
+}
