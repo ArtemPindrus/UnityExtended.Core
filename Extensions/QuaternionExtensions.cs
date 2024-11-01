9c480e1e349525528e10fd90fad6cb6ca9706fda
Artem Pindrus <artempindrus@gmail.com>
2024-11-01 09:19:14 +0200

    QuaternionExtensions

---
+++ Extensions/QuaternionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UnityExtended.Core.Extensions {
+    public static class QuaternionExtensions {
+        public static Quaternion Scale(this Quaternion q, float scalar) {
+            q.ToAngleAxis(out float angle, out Vector3 axis);
+
+            angle *= scalar;
+
+            return Quaternion.AngleAxis(angle, axis);
+        }
+    }
+}
