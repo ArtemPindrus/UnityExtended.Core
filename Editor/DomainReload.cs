e99173077ea906ad42f86d02529f677bb45eaf43
Artem Pindrus <artempindrus@gmail.com>
2024-11-09 13:39:26 +0200

    Create DomainReload.cs

---
+++ Editor/DomainReload.cs
@@ -0,0 +1,49 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UnityExtended.Core.EditorTools {
+    public class DomainReload : EditorWindow {
+        private Button reloadDomainBtn;
+        private Label topLabel;
+
+        [MenuItem(EditorToolsConst.MenuPath + nameof(DomainReload))]
+        public static void CreateWindow() {
+            DomainReload wnd = GetWindow<DomainReload>();
+            wnd.titleContent = new GUIContent("DomainReload");
+        }
+
+        public void CreateGUI() {
+            // Each editor window contains a root VisualElement object
+            VisualElement root = rootVisualElement;
+
+            topLabel = new();
+            root.Add(topLabel);
+
+            reloadDomainBtn = new(ReloadDomain);
+            reloadDomainBtn.text = "Trigger Domain Reload";
+            root.Add(reloadDomainBtn);
+
+            OnGUI();
+        }
+
+        private void OnGUI() {
+            reloadDomainBtn.SetEnabled(true);
+            topLabel.text = "";
+
+            if (Application.isPlaying) DisableAndSetText("Cannot reload in Play Mode.");
+            else if (EditorApplication.isCompiling) DisableAndSetText("Compiling scripts...");
+            else if (EditorApplication.isUpdating) DisableAndSetText("Refreshing AssetDatabase...");
+
+            void DisableAndSetText(string text) {
+                reloadDomainBtn.SetEnabled(false);
+
+                topLabel.text = text;
+            }
+        }
+
+        private void ReloadDomain() {
+            EditorUtility.RequestScriptReload();
+        }
+    }
+}
