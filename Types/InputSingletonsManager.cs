87a479c2e58a6ae6de8f1c33b97f8cfaba1c3b71
Artem Pindrus <artempindrus@gmail.com>
2024-11-09 15:41:56 +0200

    Create InputSingletonsManager.cs

---
+++ Types/InputSingletonsManager.cs
@@ -0,0 +1,35 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace UnityExtended.Core.Types {
+    public static class InputSingletonsManager {
+        private static Dictionary<Type, IInputActionCollection2>? instanceKeyedByType;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void Init() {
+            if (instanceKeyedByType != null) {
+                foreach (var value in instanceKeyedByType.Values) {
+                    value.Disable();
+                    ((IDisposable)value).Dispose();
+                } 
+            }
+
+            instanceKeyedByType = new();
+        }
+
+        public static T GetInstance<T>() where T : IInputActionCollection2, new() {
+            if (instanceKeyedByType.TryGetValue(typeof(T), out IInputActionCollection2 instance)) {
+                return (T)instance;
+            } else {
+                T newInstance = new T();
+                instanceKeyedByType.Add(typeof(T), newInstance);
+                newInstance.Enable();
+
+                return newInstance;
+            }
+        }
+    }
+}
