472ea0fa71d769856f0616ca838cc0700266b261
Artem Pindrus <artempindrus@gmail.com>
2024-11-09 17:58:36 +0200

    Create RangeFloatTests.cs

---
+++ Tests/RangeFloatTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using UnityExtended.Core.Types;
+
+namespace UnityExtended.Core.Tests {
+    public class RangeFloatTests {
+        [TestCase(-10, 10, 5, 5, 10)]
+        [TestCase(-10, 10, 10, 5, -5)]
+        [TestCase(-1, 1, 1, 2, 1)]
+        public void AdditionYieldsExpectedValues(float min, float max, float initialValue, float added, float expected) {
+            RangeFloat rf = new(min, max, initialValue);
+            
+            rf += added;
+
+            Assert.That(rf.Value, Is.EqualTo(expected));
+        }
+    }
+}
