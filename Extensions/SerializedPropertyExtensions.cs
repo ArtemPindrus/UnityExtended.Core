2be0c2c63cb5de6fca307fc469686f1b9a9450f7
Artem Pindrus <artempindrus@gmail.com>
2024-11-10 19:39:56 +0200

    Create SerializedPropertyExtensions.cs

---
+++ Extensions/SerializedPropertyExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using UnityEditor;
+
+#nullable  enable
+namespace UnityExtended.Core.Extensions {
+    public static class SerializedPropertyExtensions {
+        public static object GetValue(this SerializedProperty property) {
+            string[] pathParts = property.propertyPath.Split(".");
+            object currentObject = property.serializedObject.targetObject;
+            FieldInfo? field = null;
+
+            foreach (var part in pathParts) {
+                field = currentObject
+                    .GetType()
+                    .GetField(part, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                currentObject = field.GetValue(currentObject);
+            }
+
+            return currentObject;
+        }
+    }
+}
