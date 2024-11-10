698711b5a725242fb84bd52261390e176e7e584d
Artem Pindrus <artempindrus@gmail.com>
2024-11-10 15:04:20 +0200

    Create RangeFloatDrawer.cs

---
+++ Editor/RangeFloatDrawer.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityExtended.Core.Types;
+
+namespace UnityExtended.Core.EditorTools {
+    [CustomPropertyDrawer(typeof(RangeFloat))]
+    public class RangeFloatDrawer : PropertyDrawer {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property) {
+            VisualElement container = new();
+
+            var lowerLimitProperty = property.FindPropertyRelative("lowerLimit");
+            var upperLimitProperty = property.FindPropertyRelative("upperLimit");
+            
+            var valueField = new PropertyField(property.FindPropertyRelative("value"));
+            var lowerLimitField = new PropertyField(lowerLimitProperty);
+            var upperLimitField = new PropertyField(upperLimitProperty);
+
+            var function = typeof(RangeFloat).GetMethod("OnValueChanged", BindingFlags.Instance | BindingFlags.NonPublic);
+            var field = property.serializedObject.targetObject.GetType().GetField(property.name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+            var instance = field.GetValue(property.serializedObject.targetObject);
+            
+            valueField.RegisterValueChangeCallback((_) => function.Invoke(instance, null));
+            lowerLimitField.RegisterValueChangeCallback((_) => function.Invoke(instance, null));
+            upperLimitField.RegisterValueChangeCallback((_) => function.Invoke(instance, null));
+            
+            container.Add(valueField);
+            container.Add(lowerLimitField);
+            container.Add(upperLimitField);
+            
+            return container;
+        }
+    }
+}
