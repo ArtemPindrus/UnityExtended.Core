4f58520aeda552903cb407e3f6463489c3216019
Artem Pindrus <artempindrus@gmail.com>
2024-11-10 21:11:25 +0200

    Update SerializedPropertyExtensions.cs

--- Extensions/SerializedPropertyExtensions.cs
+++ Extensions/SerializedPropertyExtensions.cs
@@ -1,27 +1,32 @@
-﻿using System.Reflection;
+﻿using System.Linq;
+using System.Reflection;
 using UnityEditor;
 
 #nullable  enable
 namespace UnityExtended.Core.Extensions {
     public static class SerializedPropertyExtensions {
+#nullable  enable
         /// <summary>
-        /// Gets object that contains this SerializableProperty.
+        /// Gets object containing this SerializedProperty.
         /// </summary>
-        /// <param name="property"></param>
-        /// <returns></returns>
-        public static object GetContainingObject(this SerializedProperty property) {
-            string[] pathParts = property.propertyPath.Split(".");
+        /// <param name="property">SerializedProperty contained withing an instance.</param>
+        /// <returns>Object that contains given SerializedProperty.</returns>
+        public static object? GetContainingObject(this SerializedProperty property) {
+            var pathParts = property.propertyPath.Split(".").SkipLast(1);
             object currentObject = property.serializedObject.targetObject;
-            FieldInfo? field = null;
 
             foreach (var part in pathParts) {
-                field = currentObject
+                var field = currentObject
                     .GetType()
                     .GetField(part, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+                if (field == null) return null;
+				
                 currentObject = field.GetValue(currentObject);
             }
 
             return currentObject;
         }
+#nullable disable
     }
 }
