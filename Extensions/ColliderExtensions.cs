2fdbd97eee2b32f1cbe67f442b644fc64c5a220c
Artem Pindrus <artempindrus@gmail.com>
2024-12-15 12:21:44 +0200

    ColliderExtensions

---
+++ Extensions/ColliderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityExtended.Core.Extensions {
+    public static class ColliderExtensions {
+        public static Vector3 GetWorldPosition(this Collider collider) {
+            return collider switch {
+                BoxCollider box => collider.transform.position + box.center,
+                SphereCollider sphere => sphere.transform.position + sphere.center,
+                CapsuleCollider capsule => capsule.transform.position + capsule.center,
+                MeshCollider mesh => mesh.transform.position,
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+}
