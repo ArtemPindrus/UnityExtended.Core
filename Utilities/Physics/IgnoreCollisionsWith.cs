fe8e63e64ee70c0c24df8752a29b6803ceba2eff
Artem Pindrus <artempindrus@gmail.com>
2024-12-15 10:55:30 +0200

    IgnoreCollisionsWith script

---
+++ Utilities/Physics/IgnoreCollisionsWith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityExtended.Core.External.UnityExtended.Core.Utilities.Physics {
+    /// <summary>
+    /// Enforces Unity to ignore collisions between colliders (including child ones) of a GameObject with this script
+    /// and other specified colliders on Awake.
+    /// </summary>
+    public class IgnoreCollisionsWith : MonoBehaviour {
+        [SerializeField] private Collider[] otherColliders;
+        
+        private Collider[] myColliders;
+
+        private void Awake() {
+            myColliders = GetComponentsInChildren<Collider>();
+
+            foreach (var myCollider in myColliders) {
+                foreach (var other in otherColliders) {
+                    UnityEngine.Physics.IgnoreCollision(myCollider, other);
+                }
+            }
+        }
+    }
+}
