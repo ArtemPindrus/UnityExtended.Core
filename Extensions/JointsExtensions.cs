9e866efb204e6f24842f574300dda8a7094898e8
Artem Pindrus <artempindrus@gmail.com>
2024-12-23 11:49:10 +0200

    JointsExtensions

---
+++ Extensions/JointsExtensions.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+namespace UnityExtended.Core.Extensions {
+    public static class JointsExtensions {
+        public static Vector3 GetAnchorWorldPosition(this HingeJoint joint) => joint.transform.TransformPoint(joint.anchor);
+    }
+}
