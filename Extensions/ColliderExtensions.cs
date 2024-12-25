9130527b29642c24772a335fe15b77de32c35b19
Artem Pindrus <artempindrus@gmail.com>
2024-12-25 10:56:21 +0200

    DrawCollider

--- Extensions/ColliderExtensions.cs
+++ Extensions/ColliderExtensions.cs
@@ -12,5 +12,29 @@ namespace UnityExtended.Core.Extensions {
                 _ => throw new NotImplementedException()
             };
         }
+
+        public static void DrawCollider(this Collider collider, float sizeMultiplier) {
+            Vector3 worldPos = collider.GetWorldPosition();
+            
+            if (collider is BoxCollider box) Gizmos.DrawCube(worldPos, box.bounds.size * sizeMultiplier);
+            else if (collider is SphereCollider sphere) 
+                Gizmos.DrawSphere(worldPos, sphere.radius * sphere.transform.localScale.Max() * sizeMultiplier);
+            else if (collider is CapsuleCollider capsule) {
+                float halfHeight = capsule.height * capsule.transform.localScale.y / 2;
+                float radius = capsule.radius * capsule.transform.localScale.Max(true, false, true) * sizeMultiplier;
+                    
+                // upper sphere
+                Vector3 upperPos = worldPos.Add(y: halfHeight - radius);
+                Gizmos.DrawSphere(upperPos, radius);
+                    
+                // lower
+                Vector3 lowerPos = worldPos.Add(y: -halfHeight + radius);
+                Gizmos.DrawSphere(lowerPos, radius);
+            }
+            else if (collider is MeshCollider mesh) {
+                Gizmos.DrawMesh(mesh.sharedMesh, mesh.transform.position);
+            }
+            else throw new NotSupportedException();
+        }
     }
 }
