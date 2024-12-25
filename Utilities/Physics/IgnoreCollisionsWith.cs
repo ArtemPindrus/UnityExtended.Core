25a4a4a10b33601f380b6207cf1eea6ec5de5b2d
Artem Pindrus <artempindrus@gmail.com>
2024-12-25 10:57:15 +0200

    Use extension

--- Utilities/Physics/IgnoreCollisionsWith.cs
+++ Utilities/Physics/IgnoreCollisionsWith.cs
@@ -39,28 +39,7 @@ namespace UnityExtended.Core.External.UnityExtended.Core.Utilities.Physics {
             Gizmos.color = Color.red;
             
             foreach (var other in otherColliders) {
-                Vector3 worldPos = other.GetWorldPosition();
-                
-                // TODO: move drawing out?
-                if (other is BoxCollider box) Gizmos.DrawCube(worldPos, box.bounds.size * sizeMultiplier);
-                else if (other is SphereCollider sphere) 
-                    Gizmos.DrawSphere(worldPos, sphere.radius * sphere.transform.localScale.Max() * sizeMultiplier);
-                else if (other is CapsuleCollider capsule) {
-                    float halfHeight = capsule.height * capsule.transform.localScale.y / 2;
-                    float radius = capsule.radius * capsule.transform.localScale.Max(true, false, true) * sizeMultiplier;
-                    
-                    // upper sphere
-                    Vector3 upperPos = worldPos.Add(y: halfHeight - radius);
-                    Gizmos.DrawSphere(upperPos, radius);
-                    
-                    // lower
-                    Vector3 lowerPos = worldPos.Add(y: -halfHeight + radius);
-                    Gizmos.DrawSphere(lowerPos, radius);
-                }
-                else if (other is MeshCollider mesh) {
-                    Gizmos.DrawMesh(mesh.sharedMesh, mesh.transform.position);
-                }
-                else throw new NotImplementedException();
+                other.DrawCollider(sizeMultiplier);
             }
         }
     }
