f83673ba306789649c87591a664144e23575bd06
Artem Pindrus <artempindrus@gmail.com>
2025-01-23 13:52:08 +0200

    Create TimelineExtensions.cs

---
+++ Extensions/TimelineExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine.Playables;
+
+namespace UnityExtended.Core.Extensions {
+    public static class TimelineExtensions {
+        public static async UniTask PlayAndWait(this PlayableDirector director, float delta = 0) {
+            director.Play();
+
+            await UniTask.WaitForSeconds((float)director.duration + delta);
+        }
+    }
+}
