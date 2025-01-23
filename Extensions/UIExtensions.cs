59f558341ccb84e3db7d54a01f335c064062a9a8
Artem Pindrus <artempindrus@gmail.com>
2025-01-23 13:52:12 +0200

    Create UIExtensions.cs

---
+++ Extensions/UIExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Cysharp.Threading.Tasks;
+using LitMotion;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityExtended.Core.Extensions {
+    public static class UIExtensions {
+        public static void SetAlpha(this Image image, float alpha) {
+            Color currentColor = image.color;
+            currentColor.a = alpha;
+            image.color = currentColor;
+        }
+
+        public static async UniTask TweenAlpha(this Image image, float from, float to, float duration) {
+            await LMotion.Create(from, to, duration)
+                .WithCancelOnError()
+                .Bind(image, (f, i) => SetAlpha(i, f));
+        }
+        
+        public static async UniTask TweenAlpha(this TextMeshProUGUI text, float from, float to, float duration) {
+            await LMotion.Create(from, to, duration)
+                .WithCancelOnError()
+                .Bind(text, (f, i) => text.alpha = f);
+        }
+    }
+}
