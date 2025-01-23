d60d74ca29c129d9efb6057c1098781bed976699
Artem Pindrus <artempindrus@gmail.com>
2025-01-23 13:51:00 +0200

    OnUnlocked event

--- Types/Locker.cs
+++ Types/Locker.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
 
 namespace UnityExtended.Core.Types {
     public class Locker {
         private readonly HashSet<object> locks = new();
 
+        [CanBeNull] public event Action OnUnlocked;
+        
         public bool IsLocked => locks.Count > 0;
 
         public void Lock(object @lock, bool toLock) {
@@ -17,6 +21,8 @@ namespace UnityExtended.Core.Types {
 
         public void ReleaseLock(object @lock) {
             locks.Remove(@lock);
+            
+            if (locks.Count == 0) OnUnlocked?.Invoke();
         }
 
         public static implicit operator bool(Locker locker) => locker.IsLocked;
