a8f871e3bb63d5db71e467fb48fb64a91018f310
Artem Pindrus <artempindrus@gmail.com>
2025-01-30 15:13:49 +0200

    Create TaskExtensions.cs

---
+++ Extensions/TaskExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+
+namespace UnityExtended.Core.Extensions {
+    public static class TaskExtensions {
+        public static void CancelAndInstantiate(ref CancellationTokenSource cancellationTokenSource) {
+            cancellationTokenSource?.Cancel();
+            cancellationTokenSource = new();
+        }
+    }
+}
