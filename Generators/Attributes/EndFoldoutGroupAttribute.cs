df11d8ad9db7292b60d3da423f082c3918c09d7a
Artem Pindrus <artempindrus@gmail.com>
2025-02-10 19:52:34 +0200

    StartFoldoutGroupAttribute

---
+++ Generators/Attributes/EndFoldoutGroupAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace UnityExtended.Generators.Attributes {
+    [AttributeUsage(AttributeTargets.Field)]
+    public class EndFoldoutGroupAttribute : Attribute {
+        
+    }
+}
