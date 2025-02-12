76c2abcead9590662872825e4ebe6d453980ea89
Artem Pindrus <artempindrus@gmail.com>
2025-02-12 14:55:08 +0200

    DisplayItemAttribute

---
+++ Generators/Attributes/DisplayItemAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace UnityExtended.Generators.Attributes {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class DisplayItemAttribute : Attribute {
+        public DisplayItemAttribute(Type containerType, params string[] fieldsAndProperties){}
+    }
+}
