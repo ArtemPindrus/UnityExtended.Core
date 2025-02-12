6f060544a178447c98ef41217858d9fe781a57e3
Artem Pindrus <artempindrus@gmail.com>
2025-02-12 15:05:06 +0200

    Docs

--- Generators/Attributes/DisplayAttribute.cs
+++ Generators/Attributes/DisplayAttribute.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 
 namespace UnityExtended.Generators.Attributes {
+    /// <summary>
+    /// Use to display field in inspector without serialization. Creates custom inspector.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Field)]
     public class DisplayAttribute : Attribute{}
 }
--- Generators/Attributes/EndFoldoutGroupAttribute.cs
+++ Generators/Attributes/EndFoldoutGroupAttribute.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using EditorAttributes;
 
 namespace UnityExtended.Generators.Attributes {
+    /// <summary>
+    /// Stop attaching <see cref="FoldoutGroupAttribute"/> at THIS field (including).
+    /// </summary>
     [AttributeUsage(AttributeTargets.Field)]
     public class EndFoldoutGroupAttribute : Attribute {
         
