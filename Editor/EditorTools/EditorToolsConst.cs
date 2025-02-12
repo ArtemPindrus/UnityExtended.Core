003473183bf5bb35b4c73395924956f223487216
Artem Pindrus <artempindrus@gmail.com>
2025-02-12 15:10:54 +0200

    Update EditorToolsConst.cs

--- Editor/EditorTools/EditorToolsConst.cs
+++ Editor/EditorTools/EditorToolsConst.cs
@@ -2,5 +2,6 @@
     public static class EditorToolsConst {
         public const string MenuPath = "UnityExtended/";
         public const string GameObjectPath = "GameObject/Extensions/";
+        public const string AssetsPath = "Assets/Extensions/";
     }
 }
