5086063b3a7251382716bbdc0a2b236d7e6ec08a
Artem Pindrus <artempindrus@gmail.com>
2025-02-24 12:45:19 +0200

    Use AudioClip instead of Path

--- Editor/EditorTools/ToAudioResourceBetter.cs
+++ Editor/EditorTools/ToAudioResourceBetter.cs
@@ -7,15 +7,14 @@ using UnityExtended.Core.Utilities.AudioSourceBetter;
 namespace UnityExtended.Core.EditorTools {
     public static class ToAudioResourceBetter {
         public const string MenuItemPath = "Assets/Extensions/Convert to AudioResourceBetter";
-
-        // TODO: use asset object instead of assetGUIDs and paths
-        public static bool CreateAudioResourceBetterForPath(string path, [CanBeNull] out AudioResourceBetter audioResourceBetter) {
-            AudioClip clip = AssetDatabase.LoadAssetAtPath<AudioClip>(path);
+        
+        public static bool CreateAudioResourceBetterAsset(AudioClip clip, [CanBeNull] out AudioResourceBetter audioResourceBetter) {
             if (clip == null) {
                 audioResourceBetter = null;
                 return false;
             }
-        
+
+            string path = AssetDatabase.GetAssetPath(clip);
             AudioResourceBetter resourceBetter = clip;
         
             int dotIndex = path.LastIndexOf(".", StringComparison.Ordinal);
@@ -37,28 +36,13 @@ namespace UnityExtended.Core.EditorTools {
         }
     
         [MenuItem(MenuItemPath)]
-        public static void ConvertToAudioResourceBetter() {
-            foreach (var guid in Selection.assetGUIDs) {
-                var path = AssetDatabase.GUIDToAssetPath(guid);
-
-                CreateAudioResourceBetterForPath(path, out _);
+        private static void ConvertToAudioResourceBetter() {
+            foreach (var clip in Selection.objects.OfType<AudioClip>()) {
+                CreateAudioResourceBetterAsset(clip, out _);
             }
         }
 
         [MenuItem(MenuItemPath, true)]
-        public static bool ConvertToAudioResourceBetterValidate() {
-            if (Selection.assetGUIDs.Length == 0) return false;
-        
-            bool valid = true;
-        
-            foreach (var guid in Selection.assetGUIDs) {
-                var path = AssetDatabase.GUIDToAssetPath(guid);
-
-                AudioClip clip = AssetDatabase.LoadAssetAtPath<AudioClip>(path);
-                if (clip == null) valid = false;
-            }
-
-            return valid;
-        }
+        private static bool ConvertToAudioResourceBetterValidate() => Selection.objects.All(x => x is AudioClip);
     }
 }
