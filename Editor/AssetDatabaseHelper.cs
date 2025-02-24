d8da82453f01c896a7ad7135fca1075a3f3c1408
Artem Pindrus <artempindrus@gmail.com>
2025-02-24 19:05:28 +0200

    AssetDatabaseHelper

---
+++ Editor/AssetDatabaseHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityExtended.Core.Utilities.AudioSourceBetter;
+
+namespace External.UnityExtended.Core.Editor {
+    public static class AssetDatabaseHelper {
+        public static IEnumerable<T> LoadAllAssetsOfType<T>() where T : Object {
+            var assets = AssetDatabase.FindAssets($"t:{typeof(T)}");
+            
+            foreach (var e in assets) {
+                var assetPath = AssetDatabase.GUIDToAssetPath(e);
+                yield return AssetDatabase.LoadAssetAtPath<T>(assetPath);
+            }
+        }
+
+        public static T CreateAndLoadAsset<T>(T obj, string path) where T : Object {
+            AssetDatabase.CreateAsset(obj, path);
+            var asset = AssetDatabase.LoadAssetAtPath<T>(path);
+
+            return asset;
+        }
+
+    }
+}
