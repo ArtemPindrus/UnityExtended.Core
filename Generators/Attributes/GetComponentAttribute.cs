f508aaf7369872b4e4725ed567acbaa69b8c3a55
Artem Pindrus <artempindrus@gmail.com>
2025-02-25 16:48:20 +0200

    Update GetComponentAttribute.cs

--- Generators/Attributes/GetComponentAttribute.cs
+++ Generators/Attributes/GetComponentAttribute.cs
@@ -3,6 +3,12 @@ using System;
 namespace UnityExtended.Generators.Attributes {
     [AttributeUsage(AttributeTargets.Field)]
     public class GetComponentAttribute : Attribute {
+        public enum In {
+            Self,
+            Children,
+            Parent,
+        }
         
+        public GetComponentAttribute(In @in = In.Self, bool plural = false){}
     }
 }
