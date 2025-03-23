8351d318d9f92f10520e7e1f8d8858607b9644e3
Artem Pindrus <artempindrus@gmail.com>
2025-03-23 12:50:16 +0200

    Create GetComponentAheadAttribute.cs

---
+++ Generators/Attributes/GetComponentAheadAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace UnityExtended.Generators.Attributes {
+    [AttributeUsage(AttributeTargets.Field)]
+    public class GetComponentAheadAttribute : Attribute {
+        public GetComponentAheadAttribute(In @in = In.Self, bool plural = false){}
+    }
+}
