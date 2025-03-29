897b901870853468a639865f3f925d160af16517
Artem Pindrus <artempindrus@gmail.com>
2025-03-29 10:59:29 +0200

    namespace

--- Editor/AudioClipsPostprocessor.cs
+++ Editor/AudioClipsPostprocessor.cs
@@ -1,31 +1,32 @@
 using System.Linq;
-using External.UnityExtended.Core.Editor;
 using UnityEditor;
-using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityExtended.Core.Editor.EditorTools;
 using UnityExtended.Core.Utilities.AudioSourceBetter;
 
-public class AudioClipsPostprocessor : AssetPostprocessor {
-    [MenuItem("UnityExtended/Delete empty AudioResourceBetter")]
-    private static void DeleteEmptyAudioResourceBetter() {
-        var deleted = AssetDatabaseHelper.LoadAllAssetsOfType<AudioResourceBetter>()
-            .Where(x => x.resource == null);
+namespace UnityExtended.Core.Editor {
+    public class AudioClipsPostprocessor : AssetPostprocessor {
+        [MenuItem("UnityExtended/Delete empty AudioResourceBetter")]
+        private static void DeleteEmptyAudioResourceBetter() {
+            var deleted = AssetDatabaseHelper.LoadAllAssetsOfType<AudioResourceBetter>()
+                .Where(x => x.resource == null);
 
-        foreach (var d in deleted) {
-            string name = d.name;
+            foreach (var d in deleted) {
+                string name = d.name;
             
-            if (AssetDatabase.DeleteAsset(AssetDatabase.GetAssetPath(d))) {
-                Debug.Log($"Deleted {name}.");
+                if (AssetDatabase.DeleteAsset(AssetDatabase.GetAssetPath(d))) {
+                    Debug.Log($"Deleted {name}.");
+                }
             }
         }
-    }
     
-    private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
-        string[] movedFromAssetPaths) {
-        var importedAudioClips = importedAssets
-            .Select(AssetDatabase.LoadAssetAtPath<Object>)
-            .OfType<AudioClip>();
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
+            string[] movedFromAssetPaths) {
+            var importedAudioClips = importedAssets
+                .Select(AssetDatabase.LoadAssetAtPath<Object>)
+                .OfType<AudioClip>();
 
-        if (importedAudioClips.Any()) ToAudioResourceBetterWindow.Create(importedAudioClips.ToList());
+            if (importedAudioClips.Any()) ToAudioResourceBetterWindow.Create(importedAudioClips.ToList());
+        }
     }
 }
