80d0c95cdc1f7adcf32ebcc4a2780089a532d489
Artem Pindrus <artempindrus@gmail.com>
2025-03-29 10:58:10 +0200

    Create ColorHelper.cs

---
+++ Helpers/ColorHelper.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace UnityExtended.Core.Helpers {
+    public static class ColorHelper {
+        public static Color From255RGBA(float rgb, float a = 255) => 
+            new Color(rgb / 255, rgb / 255, rgb / 255, a / 255);
+        
+        public static Color From255RGBA(float r, float g, float b, float a = 255) 
+            => new Color(r / 255, g / 255, b / 255, a / 255);
+    }
+}
