302bd84fcbb96900605a9164dcf3126e2594d79c
Artem Pindrus <artempindrus@gmail.com>
2025-03-29 10:58:12 +0200

    Create ReflectionHelper.cs

---
+++ Helpers/ReflectionHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+namespace UnityExtended.Core.External.UnityExtended.Core.Helpers {
+    public static class ReflectionHelper {
+        #nullable enable
+        public static string BeautifyBackingFieldName(string name) => name.Replace("<", "").Replace(">k__BackingField", "");
+
+        public static bool TryGetCustomAttribute<T>(this MemberInfo memberInfo, out T attribute) where T : Attribute {
+            attribute = memberInfo.GetCustomAttribute<T>();
+
+            return attribute != null;
+        }
+    }
+}
