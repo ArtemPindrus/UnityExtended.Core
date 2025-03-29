0f750955d7a1b2e8b389942f79c040f74935f1c7
Artem Pindrus <artempindrus@gmail.com>
2025-03-29 12:01:22 +0200

    CreateCustomInspectorAttribute

---
+++ Generators/Attributes/CreateCustomInspectorAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace UnityExtended.Generators.Attributes {
+    /// <summary>
+    /// Marker that will force generator to create a custom inspector for this type with all inspector features included.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CreateCustomInspectorAttribute : Attribute {
+        
+    }
+}
