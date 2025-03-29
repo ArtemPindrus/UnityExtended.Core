cf3344802fe450019704428b3ce7ed72892ff32e
Artem Pindrus <artempindrus@gmail.com>
2025-03-29 10:55:19 +0200

    SetVisualElementAtDrawer

---
+++ Attributes/SetVisualElementAtAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine.UIElements;
+
+namespace UnityExtended.Core.Attributes {
+    /// <summary>
+    /// Forces <see cref="VisualElement"/>(s) to be rearranged within an inspector root VisualElement.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class SetVisualElementAtAttribute : Attribute {
+        public readonly int Order;
+
+        /// <summary>
+        /// Creates <see cref="SetVisualElementAtAttribute"/> instance.
+        /// </summary>
+        /// <param name="order">Order of decorated field's <see cref="VisualElement"/> within its inspector's root VisualElement.</param>
+        public SetVisualElementAtAttribute(int order) {
+            Order = order;
+        }
+    }
+}
---
+++ Editor/Drawers/SetVisualElementAtDrawer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityExtended.Core.Attributes;
+using UnityExtended.Core.External.UnityExtended.Core.Helpers;
+
+#nullable enable
+namespace UnityExtended.Core.Editor.Drawers {
+    public static class SetVisualElementAtDrawer {
+        /// <summary>
+        /// Rearranges <see cref="VisualElement"/> representing inspector by analyzing fields decorated with <see cref="SetVisualElementAtAttribute"/>.
+        /// </summary>
+        /// <param name="inspectorVisualElement"><see cref="VisualElement"/> representing inspector created for <paramref name="target"/>.</param>
+        /// <param name="target">Target object, for which <paramref name="inspectorVisualElement"/> is created.</param>
+        public static void Rearrange(VisualElement inspectorVisualElement, object target) {
+            var fieldInfos = target
+                .GetType()
+                .GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            List<Ordering> orderings = new();
+            foreach (var fieldInfo in fieldInfos) {
+                if (fieldInfo.TryGetCustomAttribute(out SetVisualElementAtAttribute attribute)) {
+                    var order = new Ordering(fieldInfo.Name, attribute.Order);
+                    orderings.Add(order);
+                }
+            }
+
+            orderings = orderings.OrderBy(x => x.Order).ToList();
+
+            foreach (var order in orderings) {
+                var children = inspectorVisualElement.Children();
+                
+                var child = children.FirstOrDefault(x => NormalizeChildElementName(x.name) == order.FieldName);
+                
+                if (child == null) continue;
+                
+                inspectorVisualElement.Remove(child);
+                inspectorVisualElement.Insert(order.Order, child);
+            }
+
+            static string NormalizeChildElementName(string name) {
+                return name.Replace("PropertyField:", "");
+            }
+        }
+    }
+
+    public struct Ordering {
+        public readonly string FieldName;
+        public readonly int Order;
+
+        public Ordering(string fieldName, int order) {
+            FieldName = fieldName;
+            Order = order;
+        }
+    }
+}
