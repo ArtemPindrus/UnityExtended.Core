985471930f1acb4a96d54ee5fbf97575c52f0c7e
Artem Pindrus <artempindrus@gmail.com>
2025-04-10 20:03:12 +0300

    AllowMultiple

--- src/Runtime/Generators/Attributes/HandleInputAttribute.cs
+++ src/Runtime/Generators/Attributes/HandleInputAttribute.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using UnityEngine.InputSystem;
 
+namespace UnityExtended.Core.Generators.Attributes {
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
     public class HandleInputAttribute : Attribute {
         public HandleInputAttribute(Type actionAssetType) {
         }
